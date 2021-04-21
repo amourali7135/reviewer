c262e808753a5500af34e02caefd2a6f3cc64355
Amir Mourali <amourali7135@gmail.com>
2021-04-20 19:03:22 -0500

    Controllers generated though not filled in yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,47 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'reviews/show'
+  get 'reviews/update'
+  get 'reviews/destroy'
+  get 'reviews/edit'
+  get 'perks/index'
+  get 'perks/new'
+  get 'perks/create'
+  get 'perks/show'
+  get 'perks/update'
+  get 'perks/destroy'
+  get 'perks/edit'
+  get 'feedbacks/index'
+  get 'feedbacks/new'
+  get 'feedbacks/create'
+  get 'feedbacks/show'
+  get 'feedbacks/update'
+  get 'feedbacks/destroy'
+  get 'feedbacks/edit'
+  get 'services/index'
+  get 'services/new'
+  get 'services/create'
+  get 'services/show'
+  get 'services/update'
+  get 'services/destroy'
+  get 'services/edit'
+  get 'businesses/index'
+  get 'businesses/new'
+  get 'businesses/create'
+  get 'businesses/show'
+  get 'businesses/update'
+  get 'businesses/destroy'
+  get 'businesses/edit'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Indices for services, feedback, perks, or just show on business show page?
   resources :businesses, :path => 'businesses' do
-    resources :services, except: :index, :path => 'service'
-    resources :feedbacks, except: :index
+    resources :services, :path => 'service'
+    resources :feedbacks, :path => 'customer_feedback'
     resources :perks, except: :index
     resources :reviews, except: :index
     resources :verification_qrs, only: :show, :path => 'QR_Verification' #Oh wait...lock it.
@@ -17,6 +53,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show] do
     resources :redemptions, only: [:show]
+    resources :recommendationslists
   end
 
   get 'about', to: 'pages#about', as: 'about'
