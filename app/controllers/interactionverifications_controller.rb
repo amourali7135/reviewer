a606bf52e2050126efeb4c2418c192f739a58610
Amir Mourali <amourali7135@gmail.com>
2021-05-03 19:06:43 -0500

    New versus existing interactions controller check and prototype QR code built

--- app/controllers/interactionverifications_controller.rb
+++ app/controllers/interactionverifications_controller.rb
@@ -14,26 +14,28 @@ class InteractionverificationsController < ApplicationController
 
   def show
     # @interactionverification = Interactionverification.find(params[:id])
-    ##  Or...
     @business = Business.find(params[:business_id])
-    # @interactionverification = Business.find(params[:business_id]).interactionverification
-    # @interactionverification = interactionverification.new(interactionverification_params)
     @url = request.path_info
     # raise
   end
 
   def create
-    # raise
+    raise
     @interactionverification = Interactionverification.new(interactionverification_params)
     @business = Business.find(params[:business_id])
-    if current_user.present? && params.has_key?(:user_id) && params.has_key?(:business_id) && params.has_key?(:controller) && params.has_key?(:action) && request.path_info.include?('validation') && params.has_value?("interactionverifications")
+    if
+      Interactionverification.exists?(business_id: @business.id, user_id: current_user.id)
+      flash[:alert] = "You've already verified your interaction together!"
+      redirect_to business_verificationqr_path(@business)
+    elsif
+      current_user.present? && params.has_key?(:user_id) && params.has_key?(:business_id) && params.has_key?(:controller) && params.has_key?(:action) && request.path_info.include?('validation') && params.has_value?("interactionverifications")
       @interactionverification.save
       flash[:notice] = "You've successfully verified your interaction with each other!"  #Ajax this later for the other side.
       # redirect_to @business # Or user dashboard, figure out l8r after views are up.
       redirect_to @business
     else
       flash[:alert] = "There was an error, please try again!"
-      render 'show'
+      redirect_to business_verificationqr_path(@business)
     end
   end
 
